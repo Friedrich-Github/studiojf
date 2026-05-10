dee7e193d19208538020ca15171b6f7ed02477be
Friedrich-Github <67233717+Friedrich-Github@users.noreply.github.com>
2026-05-10 22:47:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,48 @@ langButtons.forEach(button => {
     setLanguage(button.dataset.setlang);
   });
 });
+
+
+
+
+const sections = document.querySelectorAll("main section");
+const navLinks = document.querySelectorAll("nav a");
+
+function activateSectionOnScroll() {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 200;
+    const sectionHeight = section.offsetHeight;
+
+    if (
+      window.scrollY >= sectionTop &&
+      window.scrollY < sectionTop + sectionHeight
+    ) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active-section");
+
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active-section");
+    }
+  });
+}
+
+window.addEventListener("scroll", activateSectionOnScroll);
+
+/* Smooth Scroll */
+navLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const target = document.querySelector(link.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
