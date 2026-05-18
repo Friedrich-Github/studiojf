04dbdaebb6c7ec3ce916a544009d841383ff2291
Friedrich-Github <67233717+Friedrich-Github@users.noreply.github.com>
2026-05-18 14:16:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -68,3 +68,9 @@ navLinks.forEach(link => {
     }
   });
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
